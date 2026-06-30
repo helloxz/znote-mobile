ad5deb7d4bde1f3c0b602b88a6e3ed9089d380db
xiaoz <xiaoz93@outlook.com>
2026-06-30 21:24:54 +0800

    0.2.0

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.znote.app',
   appName: 'znote-mobile',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    cleartext: true   // 允许 HTTP 通信（Android 默认禁止明文流量）
+  }
 };
 
 export default config;
