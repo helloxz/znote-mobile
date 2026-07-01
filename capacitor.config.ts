d26a6095e3c68f1db3c87e64f2038b6f37c2df2f
xiaoz <xiaoz93@outlook.com>
2026-07-01 14:47:03 +0800

    修改图标

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.znote.app',
-  appName: 'znote-mobile',
+  appName: 'ZNote Mobile',
   webDir: 'dist',
   server: {
     cleartext: true   // 允许 HTTP 通信（Android 默认禁止明文流量）
